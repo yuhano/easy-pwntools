ec0ec176c8af65e2644679abbe24871b5ab15348
황상현 <sanghyeon@hwangsanghyeon-ui-MacBookPro.local>
2025-03-19 14:49:24 +0900

    main 코드 구현

--- easy_pwntools/controllers/main_controllers.py
+++ easy_pwntools/controllers/main_controllers.py
@@ -5,26 +5,36 @@ bp = Blueprint(name = 'main',
                import_name = __name__, 
                url_prefix='/')
 
+# todo : render_template 경로 설정 바꾸기
+
 @bp.route('/', methods=['GET'])
 def index():
     return redirect('/upload')
 
 @bp.route('/upload', methods=['GET'])
 def upload():
-    return render_template('upload.html')
+    return render_template('main/upload.html')
 
 @bp.route('/upload', methods=['POST'])
 def upload_file():
     return main_services.upload_file()
 
 @bp.route('/details', methods=['GET'])
-def detail():
-    return render_template('details.html')
+def details():
+    return render_template('main/details.html')
 
-@bp.route('/main', methods=['GET'])
-def main():
-    return render_template('main.html')
+@bp.route('/details', methods=['POST'])
+def input_project_information():
+    return main_services.input_project_information()
 
-@bp.route('/real_main', methods=['GET'])
-def real_main():
-    return render_template('real_main.html')
+@bp.route('/validate_details', methods=['GET'])
+def validate_details():
+    return main_services.validate_details_page()
+
+@bp.route('/validate_details2', methods=['GET'])
+def validate_details2():
+    return render_template('main/validate_details2.html')
+
+@bp.route('/menu', methods=['GET'])
+def menu():
+    return render_template('main/menu.html')
--- easy_pwntools/services/main_services.py
+++ easy_pwntools/services/main_services.py
@@ -1,6 +1,8 @@
 from flask import redirect, render_template, jsonify, request, current_app
 import os
 
+# todo : json message object화 하기
+
 def upload_file():
     if "file" not in request.files:
         return jsonify({"success": False, "message": "파일이 존재하지 않습니다."})
@@ -12,7 +14,7 @@ def upload_file():
         return jsonify({"success": False, "message": "파일 이름이 없습니다."})
     
     filename_extension = filename.split(".")[-1].lower()
-    allowed_extension = ["exe", "elf"]
+    allowed_extension = ["exe", "c"] # todo : 허용 가능한 파일 확장자 정하기
 
     if filename_extension in allowed_extension:
         file_path = os.path.join(current_app.config['UPLOAD_PATH'], file.filename)
@@ -20,3 +22,18 @@ def upload_file():
         return jsonify({"success": True, "message": "업로드 성공", "file": file.filename})
     
     return jsonify({"success": False, "message": "올바르지 않은 파일 형식입니다."})
+
+def input_project_information():
+    project_info = request.get_json()
+    project_name = project_info.get('projectName')
+    description = project_info.get('description')
+    server = project_info.get('server')
+    ctfName = project_info.get('ctfName')
+    isDockerFile = project_info.get('isDockerFile')
+    gptApiKey = project_info.get('gptApiKey')
+    fullAutoExploit = project_info.get('fullAutoExploit')
+    # todo : 위 데이터를 어떻게 처리할 것인가? ( todo2 : 데이터베이스를 사용할 것인가? )
+    return jsonify({"success": True, "message": "프로젝트 정보 입력 완료"})
+
+def validate_details_page():
+    return render_template('main/validate_details.html')
